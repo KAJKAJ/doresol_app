79101a8af865c5dfd0a6e6037b3f59e0fd545ff3
SunJang <realjangsun@gmail.com>
2014-07-22 15:27:34 +0900

    memorial routing

--- client/app/memorial/memorial.js
+++ client/app/memorial/memorial.js
@@ -3,47 +3,41 @@
 angular.module('doresolApp')
   .config(function ($stateProvider) {
     $stateProvider
-      // .state('memorial', {
-      //   url: '/memorial/:id',
-      //   templateUrl: 'app/memorial/memorial.html',
-      //   controller: 'MemorialCtrl',
-      //   authenticate: true
-      // })
-      
       .state('memorial', {
-        abstract: true,
+        // abstract: true,
         url: '/memorial/:id',
         templateUrl: 'app/memorial/memorial.html',
         controller: 'MemorialCtrl',
+        // onEnter: function(){
+        //   console.log('enter main');
+        // },
+        // onExit: function(){
+        //   console.log('exit main');
+        // },
         authenticate: true
       })
 
       .state('memorial.profile', {
-        url:'',
-        // url: '/profile',
         templateUrl: 'app/memorial/profile/profile.html',
         controller: 'ProfileCtrl',
         authenticate: true
       })
       .state('memorial.timeline', {
-        url:'',
-        // url: '/timeline',
         templateUrl: 'app/memorial/timeline/timeline.html',
         controller: 'TimelineCtrl',
         authenticate: true
       })
       .state('memorial.storyline', {
-        url:'',
-        // url: '/storyline',
         templateUrl: 'app/memorial/storyline/storyline.html',
         controller: 'StorylineCtrl',
         authenticate: true
       })
+
       .state('memorial_create', {
-        // url: '/memorial',
         templateUrl: 'app/memorial/memorial_create.html',
-        // controller: 'MemorialCtrl',
+        controller: 'MemorialCtrl',
         authenticate: true
       })      
       ;
   });
+
