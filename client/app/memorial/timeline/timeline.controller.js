81d8652782e688f701f1e7acb137dc114d15493b
SunJang <realjangsun@gmail.com>
2014-07-23 16:07:59 +0900

    refactoring variable name

--- client/app/memorial/timeline/timeline.controller.js
+++ client/app/memorial/timeline/timeline.controller.js
@@ -4,67 +4,69 @@ angular.module('doresolApp')
   .controller('TimelineCtrl', function ($scope,Util,Auth) {
     var currentUser = Auth.getCurrentUser()._id;
 
-  	$scope.dataObject = {
-          "timeline": {
-              "headline":"김학구",
-              "type":"default",
-      				"startDate":"1938,1",
-      				"text":"<i><span class='c1'></span> & <span class='c2'></span></i>",
-      				"asset": {
-                          "media":"/assets/images/father/1.png",
-                          "caption":"아버지 .. 아포 중학교 앞에서"
-                      },
-              "date": [{
-                      "startDate":"1938,12,21",
-                      "endDate":"1938,12,25",
-                      "headline":"결혼식 with 서경분",
-                      "text":"장소는 어디어디에서 결혼하게 되었음. 그리고 이렇게 되고 어쩌구 저쩌구 했었던 걸로 기억한다. 누구와 같이 갔는지는 정확히 잘 모르겠다. 어쩌구 저쩌구.. ",
-                      "asset":
-                      {
-                          "media":"/assets/images/father/1.png",
-                          "thumbnail":"/assets/images/father/1.png",
-                      }
-                  },
-              ]
-          }
-      };
+  	// $scope.dataObject = {
+   //      "timeline": {
+   //          "headline":"김학구",
+   //          "type":"default",
+   //  				"startDate":"1938,1",
+   //  				"text":"<i><span class='c1'></span> & <span class='c2'></span></i>",
+   //  				"asset": {
+   //                      "media":"/assets/images/father/1.png",
+   //                      "caption":"아버지 .. 아포 중학교 앞에서"
+   //                  },
+   //          "date": [{
+   //                  "startDate":"1938,12,21",
+   //                  "endDate":"1938,12,25",
+   //                  "headline":"결혼식 with 서경분",
+   //                  "text":"장소는 어디어디에서 결혼하게 되었음. 그리고 이렇게 되고 어쩌구 저쩌구 했었던 걸로 기억한다. 누구와 같이 갔는지는 정확히 잘 모르겠다. 어쩌구 저쩌구.. ",
+   //                  "asset":
+   //                  {
+   //                      "media":"/assets/images/father/1.png",
+   //                      "thumbnail":"/assets/images/father/1.png",
+   //                  }
+   //              },
+   //          ]
+   //      }
+   //  };
 
-      $scope.getFlowFileUniqueId = function(file){
-        return currentUser + '-' + Util.getFlowFileUniqueId(file,currentUser);
-      };
+    // createStoryJS({
+    //      type:       'timeline',
+    //      width:      '100%',
+    //      height:     '800',
+    //      source:     $scope.dataObject,
+    //      embed_id:   'timeline-embed'
+    //  });
 
-      $scope.$on('flow::fileSuccess', function (event, $flow, flowFile, message) {
-        console.log(flowFile.uniqueIdentifier); 
-      });
+    $scope.getFlowFileUniqueId = function(file){
+      return currentUser + '-' + Util.getFlowFileUniqueId(file,currentUser);
+    };
 
-      $scope.createTimeline = function(timelineForm){
-        console.log('timelineForm');
-        console.log($scope.stories);
-      };
+    $scope.$on('flow::fileSuccess', function (event, $flow, flowFile, message) {
+      console.log(flowFile.uniqueIdentifier); 
+    });
 
-      $scope.stories = [];
-      $scope.flowFileAdded = function($file){
-        $scope.stories.push(
-          {
-            new_stroy: true,
-            file: $file  
-          }
-        );
-      };
+    $scope.createTimeline = function(timelineForm){
+      console.log('timelineForm');
+      console.log($scope.stories);
+    };
 
-      $scope.flowFileDeleted = function(story){
-        var index = $scope.stories.indexOf(story);
+    $scope.stories = [];
+    $scope.flowFileAdded = function($file){
+      $scope.stories.push(
+        {
+          new_stroy: true,
+          file: $file  
+        }
+      );
+    };
 
-        // TODO: delete from remote server
-        story.file.cancel();
-        
-        $scope.stories.splice(index, 1);  
-      };
-    	// createStoryJS({
-     //      type:       'timeline',
-     //      width:      '100%',
-     //      height:     '800',
-     //      source:     $scope.dataObject,
-     //      embed_id:   'timeline-embed'
-     //  });
+    $scope.flowFileDeleted = function(story){
+      var index = $scope.stories.indexOf(story);
+
+      // TODO: delete from remote server
+      story.file.cancel();
+
+      $scope.stories.splice(index, 1);  
+    };
+    
   });
