70cd9a883eef6d6554a04f8a450e5995c7f67327
SunJang <realjangsun@gmail.com>
2014-07-25 12:53:38 +0900

    working

--- client/app/memorial/timeline/superbox.directive.js
+++ client/app/memorial/timeline/superbox.directive.js
@@ -5,41 +5,19 @@ angular.module('doresolApp')
     return {
       restrict: 'C',
       scope:{
-        story: '=story'
+        story: '=story',
       },
       templateUrl: 'app/memorial/timeline/superbox_list.html',
     
-      link: function(scope, element, attrs) {
+      link: function($scope, element, attrs) {
         element.on('click', function() {
-          if(scope.$parent.superboxToggled == scope.story.$$hashKey){
-            scope.$parent.superboxToggled = false;
-            // element.removeClass("active");
-            // element.next().css('display','none');
+          if($scope.$root.superboxToggled == $scope.story.$$hashKey){
+            $scope.$root.superboxToggled = false;
           }else{
-            scope.$parent.superboxToggled = scope.story.$$hashKey;
-            // element.parent().parent().find(".superbox-list").removeClass("active");
-            // element.parent().parent().find(".superbox-show").css('display','none');
-            
-            // element.addClass("active");
-            // element.next().css('display','block');
+            $scope.$root.superboxToggled = $scope.story.$$hashKey;            
           }
 
-          console.log(scope.story);
-          
-          // var $this = element;
-          // console.log($this);
-          // var superbox      = $('<div class="superbox-show"></div>');
-
-          // $('.superbox-list').removeClass('active');
-          // $this.addClass('active');
-          
-          // if ($(this).next().hasClass('superbox-show')) {
-          //   $('.superbox-list').removeClass('active');
-          //   superbox.toggle();
-          // } else {
-          //   superbox.insertAfter(this).css('display', 'block');
-          //   $this.addClass('active');
-          // }
+          $scope.$apply();
           
           // $('html, body').animate({
           //   scrollTop:superbox.position().top - currentimg.width()
