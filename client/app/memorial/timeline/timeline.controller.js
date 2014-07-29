3116a111ca6446e55a1c441d78d081b9ac767a48
SunJang <realjangsun@gmail.com>
2014-07-29 15:44:50 +0900

    aa

--- client/app/memorial/timeline/timeline.controller.js
+++ client/app/memorial/timeline/timeline.controller.js
@@ -118,21 +118,6 @@ angular.module('doresolApp')
       });
     };
 
-    // $scope.flowFileAdded = function($file){
-    //   $file.type = $file.file.type.split("/")[0];
-
-    //   console.log($file);
-    //   $scope.stories.push(
-    //     {
-    //       name:'제목없음',
-    //       new_story: true,
-    //       file: $file,
-    //       start_date: moment($file.file.lastModifiedDate).format("YYYY-MM-DD")
-    //       // Mon Sep 10 2012 15:19:56 GMT+0900 (KST)
-    //     }
-    //   );
-    // };
-
     $scope.flowFileDeleted = function(story){
       var index = $scope.stories.indexOf(story);
 
@@ -168,3 +153,44 @@ angular.module('doresolApp')
       });
     };
   });
+
+
+// {
+//     "timeline":
+//     {
+//         "headline":"The Main Timeline Headline Goes here",
+//         "type":"default",
+//         "text":"<p>Intro body text goes here, some HTML is ok</p>",
+//         "asset": {
+//             "media":"http://yourdomain_or_socialmedialink_goes_here.jpg",
+//             "credit":"Credit Name Goes Here",
+//             "caption":"Caption text goes here"
+//         },
+//         "date": [
+//             {
+//                 "startDate":"2011,12,10",
+//                 "endDate":"2011,12,11",
+//                 "headline":"Headline Goes Here",
+//                 "text":"<p>Body text goes here, some HTML is OK</p>",
+//                 "tag":"This is Optional",
+//                 "classname":"optionaluniqueclassnamecanbeaddedhere",
+//                 "asset": {
+//                     "media":"http://twitter.com/ArjunaSoriano/status/164181156147900416",
+//                     "thumbnail":"optional-32x32px.jpg",
+//                     "credit":"Credit Name Goes Here",
+//                     "caption":"Caption text goes here"
+//                 }
+//             }
+//         ],
+//         "era": [
+//             {
+//                 "startDate":"2011,12,10",
+//                 "endDate":"2011,12,11",
+//                 "headline":"Headline Goes Here",
+//                 "text":"<p>Body text goes here, some HTML is OK</p>",
+//                 "tag":"This is Optional"
+//             }
+
+//         ]
+//     }
+// }
