1a81de321a2531ac027b7c77f243e5e0f1d971c2
SunJang <realjangsun@gmail.com>
2014-07-31 17:11:04 +0900

    ddd

--- client/app/memorial/memorial.controller.js
+++ client/app/memorial/memorial.controller.js
@@ -9,12 +9,12 @@ angular.module('doresolApp')
     Memorial.get({id:memorialId}).$promise.then(function (value) {
       $scope.memorial = value;
       // console.log($scope);
-      // $scope.memorial.name = 'test1';
+      // $scope.memorial.name = 'test2';
       // $scope.memorial.timeline.era.push({
       // 	id:'aaa',
       // 	headline:'haha',
-      // 	startDate:'20110302',
-      // 	endDate:'20150320'
+      // 	startDate:'20110304',
+      // 	endDate:'20150325'
       // });
       // $scope.memorial.$save();
     });
