8fd45f254d1cfa42c7a373f7e7f05e6a16d5489f
SunJang <realjangsun@gmail.com>
2014-09-03 15:25:54 +0900

    public icon changed

--- client/app/memorial/profile/profile.controller.js
+++ client/app/memorial/profile/profile.controller.js
@@ -1,6 +1,57 @@
 'use strict';
 
 angular.module('doresolApp')
-  .controller('ProfileCtrl', function ($scope) {
-    // console.log('ProfileCtrl');
+  .controller('ProfileCtrl', function ($scope,$stateParams,Util,Composite, User, Memorial) {
+    $scope.today = Date.now();
+    
+    $scope.currentUser = User.getCurrentUser();
+    $scope.memorialKey = $stateParams.id;
+    $scope.memorial = Memorial.getCurrentMemorial();
+
+    // $scope.createMemorial = function(form){
+    //   if(form.$valid){
+    //     var file = null;
+    //     if($scope.newMemorial.lastUploadingFile){
+    //       file = {
+    //           location: 'local',
+    //           url: '/tmp/' + $scope.newMemorial.lastUploadingFile,
+    //           updated_at: moment().toString()
+    //         }
+    //     }
+
+    //     var memorial = {
+    //         name: $scope.newMemorial.name,
+    //         dateOfBirth: moment($scope.newMemorial.dateOfBirth).format("YYYY-MM-DD"),
+    //         dateOfDeath: moment($scope.newMemorial.dateOfDeath).format("YYYY-MM-DD"),
+    //         file:file,
+    //         ref_user:$scope.currentUser.uid,
+    //         public: $scope.newMemorial.public
+    //     };
+        
+    //     Composite.createMemorial(memorial).then(function (value) {
+    //       $state.transitionTo('memorial.timeline', {id: value.name()});
+    //     });
+    //   }
+    // }
+
+    $scope.getFlowFileUniqueId = function(file){
+      return $scope.currentUser.uid.replace(/[^\.0-9a-zA-Z_-]/img, '') + '-' + Util.getFlowFileUniqueId(file);
+    }
+   
+    $scope.$on('flow::fileSuccess', function (event, $flow, flowFile, message) {
+      $scope.fileUploading = false;
+      $scope.newMemorial.lastUploadingFile = flowFile.uniqueIdentifier;
+    });
+
+    $scope.$on('flow::fileAdded', function (event, $flow, flowFile, message) {
+      $scope.newMemorial.lastUploadingFile = null;
+    });
+
+    $scope.openDatepicker = function($event,variable) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope[variable] = true;
+
+    }
   });
