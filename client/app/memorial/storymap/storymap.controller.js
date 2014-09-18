42db30b7889649684b7711ae5c410a400d51054b
SunJang <realjangsun@gmail.com>
2014-09-18 12:24:50 +0900

    add image modal

--- client/app/memorial/storymap/storymap.controller.js
+++ client/app/memorial/storymap/storymap.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('doresolApp')
-  .controller('StorymapCtrl', function ($scope,$state,$stateParams,Memorial,ENV,$firebase,User,Composite,Comment,Util,Story,$timeout){
+  .controller('StorymapCtrl', function ($scope,$state,$stateParams,Memorial,ENV,$firebase,User,Composite,Comment,Util,Story,$timeout,$modal){
 
     $scope.mode = 'timeline';
 
@@ -173,10 +173,38 @@ angular.module('doresolApp')
 
         } else {
           TweenMax.to($slides[currentSlide], 3, {autoAlpha:0});
-          $scope.videoPlaying = false;
+          $scope.changeToGalleryMode();
           $scope.$digest();
         }
       }
+    }
+
+    $scope.changeToGalleryMode = function(){
+      $scope.videoPlaying = false;
+    }
+
+    $scope.openImageModal = function(story){
+      var modalInstance = $modal.open({
+        templateUrl: 'app/memorial/storymap/image_modal.html',
+        controller: 'ModalCtrl',
+        size: 'lg',
+        resolve: { 
+          paramFromDialogName: function(){
+            return 'story';
+          },         
+          paramFromDialogObject: function () {
+            return story;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (paramFromDialogObject) {
+        //click ok
+        // console.log('click ok');
+        // $scope.paramFromDialogObject = paramFromDialogObject;
+      }, function () {
+        //canceled
+      });
     }
 
     var currentStoriesRef =  new Firebase(ENV.FIREBASE_URI + '/memorials/'+$scope.memorialKey+'/stories');
