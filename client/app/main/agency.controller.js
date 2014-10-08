7fe6dcc4de3999de3936fc529b4209993126916b
youngmin <youngmin.k@gmail.com>
2014-10-08 12:14:44 +0900

    estimation cost

--- client/app/main/agency.controller.js
+++ client/app/main/agency.controller.js
@@ -3,4 +3,14 @@
 angular.module('doresolApp')
   .controller('AgencyCtrl', function ($scope, $modal, $http, ENV, $firebase, Memorial) {
 
+  	$scope.estimate = {};
+  	
+  	$scope.estimate.img = 100;
+  	$scope.estimate.dvd = 0;
+  	$scope.estimate.usb = 0;
+  	$scope.estimate.tot = ($scope.estimate.img * 200 + $scope.estimate.dvd * 10000 + $scope.estimate.usb * 10000) / 10000;
+
+		$scope.changeTot = function() {
+			$scope.estimate.tot = ($scope.estimate.img * 200 + $scope.estimate.dvd * 10000 + $scope.estimate.usb * 10000) / 10000;
+		}
 });
