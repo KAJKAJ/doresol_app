3f358e66aec08368185892e8cebc07a4502747be
SunJang <realjangsun@gmail.com>
2014-10-08 18:02:10 +0900

    changed

--- client/app/main/agency.controller.js
+++ client/app/main/agency.controller.js
@@ -10,13 +10,16 @@ angular.module('doresolApp')
 
   	$scope.estimate = {};
   	
-  	$scope.estimate.img = 100;
+  	$scope.estimate.img = 50;
   	$scope.estimate.dvd = 0;
   	$scope.estimate.usb = 0;
   	$scope.estimate.tot = ($scope.estimate.img * 200 + $scope.estimate.dvd * 10000 + $scope.estimate.usb * 10000) / 10000;
 
 		$scope.changeTot = function() {
-			$scope.estimate.tot = ($scope.estimate.img * 200 + $scope.estimate.dvd * 10000 + $scope.estimate.usb * 10000) / 10000;
+      var image = $scope.estimate.img / 50;
+      if(image < 1) image = 1;
+      image = Math.floor(image);
+      $scope.estimate.tot = (image*10000 + $scope.estimate.dvd * 10000 + $scope.estimate.usb * 10000) / 10000;
 		}
 
     var setUserName = function(){
